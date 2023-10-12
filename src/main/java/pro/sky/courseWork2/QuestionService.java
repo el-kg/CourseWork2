6a5da44763d349bdce237117c199b5e2a37f7c92
el-kg <kirillgalkin8@gmail.com>
2023-10-12 11:11:57 +0700

    Initial Commit

---
+++ src/main/java/pro/sky/courseWork2/QuestionService.java
@@ -0,0 +1,10 @@
+package pro.sky.courseWork2;
+import java.util.Collection;
+
+public interface QuestionService {
+    Question add(String question,String answer);
+    Question add(Question question);
+    Question remove(Question question);
+    Collection<Question> getAll();
+    Question getRandomQuestion();
+}
